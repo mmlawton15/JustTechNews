841dc2bef617ca3c0ca40922082ee8b2e8a9f8ab
Mary Margaret <mmlawton15@gmail.com>
2022-01-24 19:54:51 -0500

    still getting the error in insomina about getting or posting being impossible

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 
-router.use('/users', userRoutes);
+router.use('/api/users', userRoutes);
 
 module.exports = router;
