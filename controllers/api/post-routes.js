a3247a6de70fbd09d0fd01ddfd410c8a8e9af107
Mary Margaret <mmlawton15@gmail.com>
2022-02-03 09:59:23 -0500

    code isn't working after refactoring with lesson snapshot. weird.

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
-// const sequelize = require('../../config/connection');
-// const { Post, User, Comment, Vote } = require('../../models');
 const sequelize = require('../../config/connection');
-const { Post, User, Comment } = require('../../models');
+const { Post, User, Comment, Vote } = require('../../models');
 
-// get all users, homepage route
+// get all users
 router.get('/', (req, res) => {
+  console.log('======================');
   Post.findAll({
     attributes: [
       'id',
@@ -29,19 +28,12 @@ router.get('/', (req, res) => {
       }
     ]
   })
-    .then(dbPostData => {
-      // pass a single post object into the homepage template
-      console.log(dbPostData[0]);
-      const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('homepage', { posts });
-    })
+    .then(dbPostData => res.json(dbPostData))
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
 });
-
-
 
 router.get('/:id', (req, res) => {
   Post.findOne({
@@ -85,26 +77,30 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
-  Post.create({
-    title: req.body.title,
-    post_url: req.body.post_url,
-    user_id: req.body.user_id
-  })
-    .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  if (req.session) {
+    Post.create({
+      title: req.body.title,
+      post_url: req.body.post_url,
+      user_id: req.session.user_id
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 router.put('/upvote', (req, res) => {
   // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote, Comment, User })
-    .then(updatedVoteData => res.json(updatedVoteData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  if (req.session) {
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+      .then(updatedVoteData => res.json(updatedVoteData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 router.put('/:id', (req, res) => {
@@ -132,6 +128,7 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
+  console.log('id', req.params.id);
   Post.destroy({
     where: {
       id: req.params.id
